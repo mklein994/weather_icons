9532e53f85bdd8952e9bd4fab5aafefd991651be
Matthew Klein <mklein994@gmail.com>
2018-02-05 12:50:13 -0600

    Convert dripicons into a list of chars
    Convert the enum index into a char by using its index as a unicode code
    point.

--- src/dripicon.rs
+++ src/dripicon.rs
@@ -1,200 +1,195 @@
 use super::icon::Icon;
 use std::fmt;
 
+#[derive(Debug, Copy, Clone)]
 pub enum DripIcon {
-    Cloud,
-    CloudDrizzle,
-    CloudDrizzleLightning,
-    CloudDrizzleLightningMoon,
-    CloudDrizzleLightningSun,
-    CloudDrizzleLightningSunAlt,
-    CloudDrizzleMoon,
-    CloudDrizzleSun,
-    CloudDrizzleSunAlt,
-    CloudFog,
-    CloudFogAlt,
-    CloudFogMoon,
-    CloudFogSun,
-    CloudFogSunAlt,
-    CloudHail,
-    CloudHailLightning,
-    CloudHailLightningMoon,
-    CloudHailLightningSun,
-    CloudHailLightningSunAlt,
-    CloudHailMoon,
-    CloudHailSun,
-    CloudHailSunAlt,
-    CloudLightning,
-    CloudLightningMoon,
-    CloudLightningSun,
-    CloudLightningSunAlt,
-    CloudMoon,
-    CloudRain,
-    CloudRainAlt,
-    CloudRainAltMoon,
-    CloudRainAltSun,
-    CloudRainAltSunAlt,
-    CloudRainLightning,
-    CloudRainLightningMoon,
-    CloudRainLightningSun,
-    CloudRainLightningSunAlt,
-    CloudRainMoon,
-    CloudRainSun,
-    CloudRainSunAlt,
-    CloudSnow,
-    CloudSnowMoon,
-    CloudSnowSun,
-    CloudSnowSunAlt,
-    CloudSun,
-    CloudSunAlt,
-    CloudWind,
-    CloudWindAlt,
-    CloudWindAltMoon,
-    CloudWindAltSun,
-    CloudWindAltSunAlt,
-    CloudWindMoon,
-    CloudWindSun,
-    CloudWindSunAlt,
-    Clouds,
-    CloudsMoon,
-    CloudsSun,
-    CloudsSunAlt,
-    CompassEast,
-    CompassNorth,
-    CompassSouth,
-    CompassWest,
-    DegreesCelsius,
-    DegreesFahrenheit,
-    Drizzle,
-    Flag,
-    Fog,
-    Hail,
-    Lightning,
-    Moon25,
-    Moon50,
-    Moon75,
-    Moon100,
-    MoonStars,
-    Rain,
-    Raindrop,
-    Snow,
-    Sun,
-    SunEclipse,
-    SunLow,
-    SunLower,
-    SunRise,
-    SunSet,
-    Thermometer25,
-    Thermometer50,
-    Thermometer75,
-    Thermometer100,
-    Tornado,
-    Umbrella,
-    UmbrellaDrizzle,
-    Wet,
-    Wind,
+    /// a
+    Cloud = 0x61,
+    /// b
+    CloudDrizzle = 0x62,
+    /// c
+    CloudDrizzleLightning = 0x63,
+    /// d
+    CloudDrizzleLightningMoon = 0x64,
+    /// e
+    CloudDrizzleLightningSun = 0x65,
+    /// f
+    CloudDrizzleLightningSunAlt = 0x66,
+    /// g
+    CloudDrizzleMoon = 0x67,
+    /// h
+    CloudDrizzleSun = 0x68,
+    /// i
+    CloudDrizzleSunAlt = 0x69,
+    /// j
+    CloudFog = 0x6a,
+    /// k
+    CloudFogAlt = 0x6b,
+    /// l
+    CloudFogMoon = 0x6c,
+    /// m
+    CloudFogSun = 0x6d,
+    /// n
+    CloudFogSunAlt = 0x6e,
+    /// o
+    CloudHail = 0x6f,
+    /// p
+    CloudHailLightning = 0x70,
+    /// q
+    CloudHailLightningMoon = 0x71,
+    /// r
+    CloudHailLightningSun = 0x72,
+    /// s
+    CloudHailLightningSunAlt = 0x73,
+    /// t
+    CloudHailMoon = 0x74,
+    /// u
+    CloudHailSun = 0x75,
+    /// v
+    CloudHailSunAlt = 0x76,
+    /// w
+    CloudLightning = 0x77,
+    /// x
+    CloudLightningMoon = 0x78,
+    /// y
+    CloudLightningSun = 0x79,
+    /// z
+    CloudLightningSunAlt = 0x7a,
+    /// A
+    CloudMoon = 0x41,
+    /// B
+    CloudRain = 0x42,
+    /// C
+    CloudRainAlt = 0x43,
+    /// D
+    CloudRainAltMoon = 0x44,
+    /// E
+    CloudRainAltSun = 0x45,
+    /// F
+    CloudRainAltSunAlt = 0x46,
+    /// G
+    CloudRainLightning = 0x47,
+    /// H
+    CloudRainLightningMoon = 0x48,
+    /// I
+    CloudRainLightningSun = 0x49,
+    /// J
+    CloudRainLightningSunAlt = 0x4a,
+    /// K
+    CloudRainMoon = 0x4b,
+    /// L
+    CloudRainSun = 0x4c,
+    /// M
+    CloudRainSunAlt = 0x4d,
+    /// N
+    CloudSnow = 0x4e,
+    /// O
+    CloudSnowMoon = 0x4f,
+    /// P
+    CloudSnowSun = 0x50,
+    /// Q
+    CloudSnowSunAlt = 0x51,
+    /// R
+    CloudSun = 0x52,
+    /// S
+    CloudSunAlt = 0x53,
+    /// T
+    CloudWind = 0x54,
+    /// U
+    CloudWindAlt = 0x55,
+    /// V
+    CloudWindAltMoon = 0x56,
+    /// W
+    CloudWindAltSun = 0x57,
+    /// X
+    CloudWindAltSunAlt = 0x58,
+    /// Y
+    CloudWindMoon = 0x59,
+    /// Z
+    CloudWindSun = 0x5a,
+    /// 0
+    CloudWindSunAlt = 0x30,
+    /// 1
+    Clouds = 0x31,
+    /// 2
+    CloudsMoon = 0x32,
+    /// 3
+    CloudsSun = 0x33,
+    /// 4
+    CloudsSunAlt = 0x34,
+    /// 5
+    CompassEast = 0x35,
+    /// 6
+    CompassNorth = 0x36,
+    /// 7
+    CompassSouth = 0x37,
+    /// 8
+    CompassWest = 0x38,
+    /// 9
+    DegreesCelsius = 0x39,
+    /// !
+    DegreesFahrenheit = 0x21,
+    /// "
+    Drizzle = 0x22,
+    /// #
+    Flag = 0x23,
+    /// $
+    Fog = 0x24,
+    /// %
+    Hail = 0x25,
+    /// &
+    Lightning = 0x26,
+    /// '
+    Moon25 = 0x27,
+    /// (
+    Moon50 = 0x28,
+    /// )
+    Moon75 = 0x29,
+    /// *
+    Moon100 = 0x2a,
+    /// +
+    MoonStars = 0x2b,
+    /// ,
+    Rain = 0x2c,
+    /// -
+    Raindrop = 0x2d,
+    /// .
+    Snow = 0x2e,
+    /// /
+    Sun = 0x2f,
+    /// :
+    SunEclipse = 0x3a,
+    /// ;
+    SunLow = 0x3b,
+    /// <
+    SunLower = 0x3c,
+    /// =
+    SunRise = 0x3d,
+    /// >
+    SunSet = 0x3e,
+    /// ?
+    Thermometer25 = 0x3f,
+    /// @
+    Thermometer50 = 0x40,
+    /// [
+    Thermometer75 = 0x5b,
+    /// ]
+    Thermometer100 = 0x5d,
+    /// ^
+    Tornado = 0x5e,
+    /// _
+    Umbrella = 0x5f,
+    /// `
+    UmbrellaDrizzle = 0x60,
+    /// {
+    Wet = 0x7b,
+    /// |
+    Wind = 0x7c,
 }
 
 impl fmt::Display for DripIcon {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        use self::DripIcon::*;
-        write!(
-            f,
-            "{}",
-            match *self {
-                Cloud => "a",
-                CloudDrizzle => "b",
-                CloudDrizzleLightning => "c",
-                CloudDrizzleLightningMoon => "d",
-                CloudDrizzleLightningSun => "e",
-                CloudDrizzleLightningSunAlt => "f",
-                CloudDrizzleMoon => "g",
-                CloudDrizzleSun => "h",
-                CloudDrizzleSunAlt => "i",
-                CloudFog => "j",
-                CloudFogAlt => "k",
-                CloudFogMoon => "l",
-                CloudFogSun => "m",
-                CloudFogSunAlt => "n",
-                CloudHail => "o",
-                CloudHailLightning => "p",
-                CloudHailLightningMoon => "q",
-                CloudHailLightningSun => "r",
-                CloudHailLightningSunAlt => "s",
-                CloudHailMoon => "t",
-                CloudHailSun => "u",
-                CloudHailSunAlt => "v",
-                CloudLightning => "w",
-                CloudLightningMoon => "x",
-                CloudLightningSun => "y",
-                CloudLightningSunAlt => "z",
-                CloudMoon => "A",
-                CloudRain => "B",
-                CloudRainAlt => "C",
-                CloudRainAltMoon => "D",
-                CloudRainAltSun => "E",
-                CloudRainAltSunAlt => "F",
-                CloudRainLightning => "G",
-                CloudRainLightningMoon => "H",
-                CloudRainLightningSun => "I",
-                CloudRainLightningSunAlt => "J",
-                CloudRainMoon => "K",
-                CloudRainSun => "L",
-                CloudRainSunAlt => "M",
-                CloudSnow => "N",
-                CloudSnowMoon => "O",
-                CloudSnowSun => "P",
-                CloudSnowSunAlt => "Q",
-                CloudSun => "R",
-                CloudSunAlt => "S",
-                CloudWind => "T",
-                CloudWindAlt => "U",
-                CloudWindAltMoon => "V",
-                CloudWindAltSun => "W",
-                CloudWindAltSunAlt => "X",
-                CloudWindMoon => "Y",
-                CloudWindSun => "Z",
-                CloudWindSunAlt => "0",
-                Clouds => "1",
-                CloudsMoon => "2",
-                CloudsSun => "3",
-                CloudsSunAlt => "4",
-                CompassEast => "5",
-                CompassNorth => "6",
-                CompassSouth => "7",
-                CompassWest => "8",
-                DegreesCelsius => "9",
-                DegreesFahrenheit => "!",
-                Drizzle => "\"",
-                Flag => "#",
-                Fog => "$",
-                Hail => "%",
-                Lightning => "&",
-                Moon25 => "'",
-                Moon50 => "(",
-                Moon75 => ")",
-                Moon100 => "*",
-                MoonStars => "+",
-                Rain => ",",
-                Raindrop => "-",
-                Snow => ".",
-                Sun => "/",
-                SunEclipse => ":",
-                SunLow => ";",
-                SunLower => "<",
-                SunRise => "=",
-                SunSet => ">",
-                Thermometer25 => "?",
-                Thermometer50 => "@",
-                Thermometer75 => "[",
-                Thermometer100 => "]",
-                Tornado => "^",
-                Umbrella => "_",
-                UmbrellaDrizzle => "`",
-                Wet => "{",
-                Wind => "|",
-            }
-        )
+        write!(f, "{}", ::std::char::from_u32(*self as u32).unwrap())
     }
 }
 
